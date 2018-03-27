85db525d66cf9f0e99ca17e058605ed2f5e10cc2
mnindrazaka <mnindrazaka@gmail.com>
2018-03-27 11:57:27 +0700

    first game scene complete

---
+++ Assets/Scripts/CatcherController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatcherController : MonoBehaviour {
+
+	private CatcherDisplay catcherDisplay;
+	private int totalEgg = 0;
+
+	void Start() {
+		catcherDisplay = GetComponent<CatcherDisplay> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D hit) {
+		if (hit.tag == "Egg") {
+			totalEgg++;
+			catcherDisplay.OnChangeEggTotal (totalEgg);
+		}
+	}
+}
---
+++ Assets/Scripts/CatcherDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatcherDisplay : MonoBehaviour {
+
+	public Image[] eggPlaceholders;
+	public Sprite iconEggFilled;
+	public Sprite iconEggEmpty;
+
+	public void OnChangeEggTotal(int total) {
+		for(int i = 0; i < eggPlaceholders.Length; i++) {
+			if (i < total) {
+				eggPlaceholders [i].sprite = iconEggFilled;
+			} else {
+				eggPlaceholders [i].sprite = iconEggEmpty;
+			}
+		}
+	}
+}
